b761b99dccbdbebf2e59e4d9186b391ffc054511
Marianna Ramos <marirsas.pt@gmail.com>
2025-01-27 13:38:45 +0000

    Att 1.0
    Created Sale Model

---
+++ AuctionHouse.WebAPI/Models/Sale.cs
@@ -0,0 +1,17 @@
+﻿namespace AuctionHouse.WebAPI.Models {
+    public class Sale {
+
+        public int Id { get; set; }
+
+
+        public int ItemId { get; set; }
+
+
+        public DateOnly DateOfSale { get; set; }
+
+
+        public decimal SalePrice { get; set; }
+
+
+    }
+}
